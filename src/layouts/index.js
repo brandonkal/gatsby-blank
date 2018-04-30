38f579a3c73ff125174757ce3c5c5c436c51e6c3
popwebz <bwebservice@gmail.com>
2018-04-29 19:17:10 -0500

    Finish Setting styling
    Looks good!

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-
 import './index.css'
 
 const Layout = ({ children, data }) => (
@@ -12,7 +11,7 @@ const Layout = ({ children, data }) => (
       title={data.site.siteMetadata.title}
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
@@ -21,7 +20,7 @@ const Layout = ({ children, data }) => (
         margin: '0 auto',
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        paddingTop: 0
       }}
     >
       {children()}
@@ -30,7 +29,7 @@ const Layout = ({ children, data }) => (
 )
 
 Layout.propTypes = {
-  children: PropTypes.func,
+  children: PropTypes.func
 }
 
 export default Layout
