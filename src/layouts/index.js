716710bcb2b98ababf3d744440c46c4e2d35ffce
Brandon Kalinowski <bwebservice@gmail.com>
2018-05-04 00:21:29 -0700

    Add bad commit Jim for testing lint

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import jim from 'jim'
 
 import Header from '../components/header'
 import './index.css'
