12998ab3929d014092d2554cdf0706dc4deb4740
PrettierCI <commits@prettierci.com>
2018-05-04 08:12:08 +0000

    Sync with Prettier

--- src/layouts/demo-eslint.js
+++ src/layouts/demo-eslint.js
@@ -1,26 +1,26 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import Link from "gatsby-link";
-import Header from "../components/header";
-import "./index.css";
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
+import Header from '../components/header'
+import './index.css'
 
 const Demo = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
     <div
       style={{
-        margin: "0 auto",
+        margin: '0 auto',
         maxWidth: 960,
-        padding: "0px 1.0875rem 1.45rem",
-        paddingTop: 0
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
       }}
     >
       <Link to="home" style="font-size: 16pt">
@@ -29,13 +29,13 @@ const Demo = ({ children, data }) => (
       {children()}
     </div>
   </div>
-);
+)
 
 Demo.propTypes = {
-  children: PropTypes.func
-};
+  children: PropTypes.func,
+}
 
-export default Demo;
+export default Demo
 
 export const query = graphql`
   query SiteTitleQueryDemo {
@@ -45,4 +45,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
